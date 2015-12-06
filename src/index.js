a2bf47be2c654df8f1b20902134d50039cf85390
Jim Hoskins <jim@jimhoskins.com>
2015-12-05 17:21:26 -0800

    Added multi-command statement parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { HueApi, lightState } from 'node-hue-api';
 import SlackClient from './SlackClient';
+import Color from 'color'
 
 const {
   SLACK_TOKEN,
@@ -10,13 +11,31 @@ const {
 const hue = new HueApi(HUE_IP, HUE_USERNAME);
 const client = new SlackClient(SLACK_TOKEN);
 
-client.register(/light (\d+|\*) (on|off)/, ([light, state]) => {
-  const s = lightState.create();
-  if (state === 'on') {
-    s.on();
-  } else {
-    s.off();
-  }
+const buildLightState = (stateString) => {
+  const state = lightState.create();
+  stateString.toLowerCase().split(/\s+/).forEach((command) => {
+    let match;
+
+    // on|off
+    if (match = command.match(/(on|off)/)) {
+      state.on(match[1] == 'on');
+    }
+
+    // 0% to 100%
+    if (match = command.match(/(\d+)%/)) {
+      state.bri(2.55 * Math.min(100, Math.max(0, parseInt(match[1], 10))));
+    }
+
+    // #RRGGBB
+    if (match = command.match(/(#[a-f0-9]{6})/)) {
+      state.rgb(Color(match[1]).rgbArray());
+    }
+  });
+  return state;
+}
+
+client.register(/light (\d+|\*) (.*)/, ([light, stateString]) => {
+  const s = buildLightState(stateString);
 
   // Change all lights:
   if (light === '*') {
